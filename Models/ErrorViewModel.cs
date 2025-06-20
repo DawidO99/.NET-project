8c6b9e0d1be23aca69a145c1d1ab6ade6c52554d
Dawid Ochman <dawid.ochman99@gmail.com>
2025-06-20 09:46:17 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarWorkshopManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
