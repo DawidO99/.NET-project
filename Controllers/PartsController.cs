12bde59f96bf7474ddec2c536511e8f8b06bb493
unknown <gabrielablaut05@gmail.com>
2025-06-21 22:13:08 +0200

    crud czesci

--- Controllers/PartsController.cs
+++ Controllers/PartsController.cs
@@ -1,11 +1,14 @@
-﻿using CarWorkshopManagementSystem.Models;
+﻿// Controllers/PartsController.cs
+using CarWorkshopManagementSystem.Models;
 using CarWorkshopManagementSystem.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; // Dodaj to using dla DbUpdateConcurrencyException
 using System.Threading.Tasks;
 
 namespace CarWorkshopManagementSystem.Controllers
 {
+    // Autoryzacja: tylko Admin i Recepcjonista mogą zarządzać częściami
     [Authorize(Roles = "Admin,Recepcjonista")]
     public class PartsController : Controller
     {
@@ -19,7 +22,24 @@ namespace CarWorkshopManagementSystem.Controllers
         // GET: Parts
         public async Task<IActionResult> Index()
         {
-            return View(await _partService.GetAllPartsAsync());
+            var parts = await _partService.GetAllPartsAsync();
+            return View(parts);
+        }
+
+        // GET: Parts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _partService.GetPartByIdAsync(id.Value);
+            if (part == null)
+            {
+                return NotFound();
+            }
+            return View(part);
         }
 
         // GET: Parts/Create
@@ -44,9 +64,16 @@ namespace CarWorkshopManagementSystem.Controllers
         // GET: Parts/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null) return NotFound();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
             var part = await _partService.GetPartByIdAsync(id.Value);
-            if (part == null) return NotFound();
+            if (part == null)
+            {
+                return NotFound();
+            }
             return View(part);
         }
 
@@ -55,16 +82,59 @@ namespace CarWorkshopManagementSystem.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Type,UnitPrice")] Part part)
         {
-            if (id != part.Id) return NotFound();
+            if (id != part.Id)
+            {
+                return NotFound();
+            }
 
             if (ModelState.IsValid)
             {
-                await _partService.UpdatePartAsync(part);
+                try
+                {
+                    await _partService.UpdatePartAsync(part);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    // Dodaj logikę obsługi konfliktu współbieżności, jeśli będzie potrzebna
+                    // np. sprawdzenie, czy element nadal istnieje
+                    if (await _partService.GetPartByIdAsync(part.Id) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(part);
         }
 
-        // Inne akcje jak Delete dodasz w ten sam sposób
+        // GET: Parts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _partService.GetPartByIdAsync(id.Value);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // POST: Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _partService.DeletePartAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
